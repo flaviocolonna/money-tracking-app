0660d5eb7473b2a6fecbf94aa736cc1dbdad6aa3
Flavio Colonna <colonnaromanoflavio@gmail.com>
2021-01-22 19:51:13 +0100

    Create index html along with index js. Create first skeleton of the Wallet model.

---
+++ src/js/models/Wallet.js
@@ -0,0 +1,19 @@
+function Wallet() {
+    var balance = 0;
+    var operations = [];
+    this.addOperation = function() {
+
+    }
+    this.removeOperation = function() {
+    
+    }
+    this.findOperation = function() {
+    
+    }
+    this.getBalance = function() {
+        return balance;
+    }
+    this.getOperations = function() {
+        return operations;
+    }
+}
