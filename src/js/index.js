dd3560584a6b5741252b625a20dfda8e6a7cc7dd
Flavio Colonna <colonnaromanoflavio@gmail.com>
2021-02-19 19:42:17 +0100

    Add snackbar functionality

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,29 @@
 const Wallet = require("./models/Wallet").Wallet;
+const Enums = require("./models/enums");
 
 let wallet;
 
-const resetFormFields = function(form) {
+const hideSnackbar = function() {
+    const toastElement = document.getElementById('toast');
+    toastElement.classList.remove('show');
+    toastElement.classList.remove('toast--error');
+}
+const showMessage = function (msg, type) {
+    const toastElement = document.getElementById('toast');
+    if (!toastElement || !msg || !Enums.SnackbarTypes[type]) {
+        return;
+    }
+    if(type === Enums.SnackbarTypes.ERROR) {
+        toastElement.classList.add('toast--error');
+    }
+    const messageElement = toastElement.querySelector('.toast__message');
+    messageElement.textContent = msg;
+    toastElement.classList.add('show');
+    setTimeout(function() {
+        hideSnackbar();
+    }, 5000);
+}
+const resetFormFields = function (form) {
     const amountInput = form.amount;
     const descriptionInput = form.description
     amountInput.value = 0;
@@ -23,8 +44,10 @@ const addOperation = function (ev) {
         wallet.addOperation(operation);
         toggleModal();
         resetFormFields(ev.target);
+        showMessage('Operation added successfully!', Enums.SnackbarTypes.SUCCESS);
     } catch (e) {
         console.error(e);
+        showMessage('Operation not added!', Enums.SnackbarTypes.ERROR);
     }
 }
 const removeOperation = function (id) {
@@ -43,18 +66,19 @@ const getBalance = function () {
 const getOperations = function () {
     return wallet.getOperations();
 }
-const toggleModal = function() {
+const toggleModal = function () {
     const modalComponent = document.getElementById('modal');
-    if(!modalComponent) {
+    if (!modalComponent) {
         return;
     }
     const isHidden = modalComponent.classList.contains('hide');
-    if(isHidden) {
+    if (isHidden) {
         modalComponent.classList.remove('hide');
         return;
     }
     modalComponent.classList.add('hide');
 }
+window.hideSnackbar = hideSnackbar;
 window.addOperation = addOperation;
 window.toggleModal = toggleModal;
 window.addEventListener('DOMContentLoaded', function () {
--- src/js/models/enums.js
+++ src/js/models/enums.js
@@ -6,8 +6,12 @@ const WalletErrors = Object.freeze({
     INVALID_OPERATION: 'INVALID_OPERATION',
     OPERATION_NOT_FOUND: 'OPERATION_NOT_FOUND'
 });
-
+const SnackbarTypes = Object.freeze({
+    SUCCESS: 'SUCCESS',
+    ERROR: 'ERROR'
+});
 module.exports = {
     OpType: OpType,
-    WalletErrors: WalletErrors
+    WalletErrors: WalletErrors,
+    SnackbarTypes: SnackbarTypes
 }
