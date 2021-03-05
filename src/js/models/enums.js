d1cc3dbd209d98d8ad4fb363c4b9c5dac628f207
Flavio Colonna <colonnaromanoflavio@gmail.com>
2021-03-05 17:02:24 +0100

    Add jsdoc

--- src/js/models/enums.js
+++ src/js/models/enums.js
@@ -1,3 +1,9 @@
+/**
+ * @typedef {string} SnackbarType
+ * @property {string} SUCCESS - Show green snackbar
+ * @property {string} ERROR - Show red snackbar
+ */
+
 const OpType = Object.freeze({
     OUT: 'OUT',
     IN: 'IN',
@@ -6,6 +12,10 @@ const WalletErrors = Object.freeze({
     INVALID_OPERATION: 'INVALID_OPERATION',
     OPERATION_NOT_FOUND: 'OPERATION_NOT_FOUND',
 });
+/**
+ * @readonly
+ * @enum {SnackbarType}
+ */
 const SnackbarTypes = Object.freeze({
     SUCCESS: 'SUCCESS',
     ERROR: 'ERROR',
