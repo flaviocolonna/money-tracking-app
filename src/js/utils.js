be977d79abb6b0c23271d8ac0349f95a666bdbad
Flavio Colonna <colonnaromanoflavio@gmail.com>
2021-05-03 08:54:03 +0200

    Update jsdoc

--- src/js/utils.js
+++ src/js/utils.js
@@ -31,11 +31,27 @@ async function getWallet() {
     }
     return wallet;
 }
+/**
+ * This function calls the endpoint POST_OPERATION to perform a post
+ * in order to create a new operation entity
+ * @function
+ * @name doCreateOperation
+ * @param {Operation} operation
+ * @return {Promise}
+ */
 const doCreateOperation = (operation) =>
     axios.post(
         `${ConfigService.getApiBaseEndpoint()}/${Endpoints.POST_OPERATION}`,
         operation
     );
+/**
+ * This function calls the endpoint related to an operation to perform a delete call
+ * in order to remove an operation entity
+ * @function
+ * @name doRemoveOperation
+ * @param {number} operationID
+ * @return {Promise}
+ */
 const doRemoveOperation = (operationID) =>
     axios.delete(
         `${ConfigService.getApiBaseEndpoint()}/${
