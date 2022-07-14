ef28d8d18d7ca30879e3fe4086d62dd6a4f29588
Flavio Colonna <colonnaromanoflavio@gmail.com>
2022-07-14 16:10:44 +0200

    Update fastify, fastify-cors and axios

--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
 const fastify = require('fastify')({
     logger: true,
 });
-fastify.register(require('fastify-cors'), {
+fastify.register(require('@fastify/cors'), {
     origin: '*',
 });
 fastify.register(require('./api'), { prefix: '/api' });
-fastify.listen(9000, function (err, address) {
-    if (err) {
-        throw err;
+const startServer = async () => {
+    try {
+        await fastify.listen({ port: 9000 });
+        fastify.log.info('Server started on 9000');
+    } catch (e) {
+        fastify.log.error(e);
+        process.exit(1);
     }
-    fastify.log.info(`Server started on ${address}`);
-});
+};
+startServer();
